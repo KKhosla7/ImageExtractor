4b82f4732cb4449c3f08df3d1bc6e822db80bcf8
karan.khosla <karan_khosla@outlook.com>
2014-02-14 14:17:34 +0530

    removed main.

--- src/http/SlideShare.java
+++ src/http/SlideShare.java
@@ -16,11 +16,6 @@ public class SlideShare {
 
   private static final String USER_AGENT = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; .NET CLR 2.0.50727; MS-RTC LM 8)";
 
-  public static void main(String[] args) throws IOException {
-    SlideShare slideShare = new SlideShare();
-    slideShare.getAllSlidesInList("http://www.slideshare.net/alexcheng1982/java-se-7-new-features-and-enhancements?v=default&b=&from_search=1");
-  }
-
   private List<String> listAllSlides(String url) {
     List<String> slideURList = new ArrayList<>();
     Document htmlDocument;
